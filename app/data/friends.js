ceb126b6299c455d58fdc8792b291f270ddb7058
Jannah June Nieves <jannahjune@gmail.com>
2019-09-21 09:20:43 -0700

    modal not working

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+	{
+		name: "Tony",
+		photo: "https://www.thewrap.com/wp-content/uploads/2017/07/Robert-Downey-Jr-Iron-Man-Pepper-Potts-Tony-Stark.jpg",
+		scores: [2, 1, 4, 5, 1, 3, 2, 5, 2, 1],
+		message: "He's complicated, a little self-absorbed, and sometimes moody. Tony is perceptive enough to identify his shortcomings and find people to shore up his weaknesses"
+	},
+	{
+		name: "Steve",
+		photo: "https://66.media.tumblr.com/fa226c7440dcb6078cf382cec4068b44/tumblr_p84q7wSj8P1tk2oito3_250.png",
+		scores: [2, 4, 5, 1, 2, 2, 3, 5, 3, 3],
+		message: "Steve is a good man, but one who often feels pressure to be the unstoppable soldier everyone wants him to be, and the two things don't always fit together.  Steve believes in doing the right thing above all else"
+	},
+	{
+		name: "Diana",
+		photo: "https://66.media.tumblr.com/ceedc6cdda2a161becc9a95d91fc4ea9/tumblr_ozbkb43DP11wd56hno5_250.png",
+		scores: [1, 2, 1, 3, 1, 5, 3, 2, 3, 4],
+		message: "Diana is a compassionate caring, stubborn, opinionated, highly competitive, outgoing, immortal Amazon. She tries to avoid conflict but if pressed she will engage in battle and on occasion lose herself in the pleasure of battle."
+	},
+	{
+		name: "Arthur",
+		photo: "https://cdn.arstechnica.net/wp-content/uploads/2018/11/aquaALTTOP-800x542.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5],
+		message: "Arthur is adventurous, and when inclined to do good, acts as an enthusiastic helper. He is highly adaptable and always knows hoe to lighten the mood with a joke or two."
+	},
+	{
+		name: "Clark",
+		photo: "https://i.pinimg.com/originals/35/f5/8c/35f58c7389974c04e3daa8c47d46de6a.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3],
+		message: "Clark expresses a resolutely noble personality, so much so that it can be hard to find noticeable flaws in him. He's gentle, kindhearted and selfless. He is also resolute, with a firm knowledge of right and wrong and an ability to act decisively in a crisis."
+	}
+];
+
+module.exports = friends;
