510a0740e9f3347823d96e7416996df03fcac400
Jannah June Nieves <jannahjune@gmail.com>
2019-09-22 22:31:13 -0700

    middleware for css

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/app/css"));
+// app.use(express.static(path.join(__dirname + "/app/css")));
+app.use(express.static("app/public"));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
